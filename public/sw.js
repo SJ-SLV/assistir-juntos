e8722f59b529403899c56ec91972db570cface80
First Step <trabalhocanva07@gmail.com>
2026-09-20 21:17:34 +0100

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = '2on-v5';
+const CACHE = '2on-v5.1';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', e => {
